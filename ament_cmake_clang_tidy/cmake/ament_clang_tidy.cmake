1b78a5def9e4c174aaf00eb5a30baab30989be9d
Rod Taylor <rod.taylor@csiro.au>
2026-01-30 01:41:48 +0000

    Bump timeout to 20 minutes.

--- ament_cmake_clang_tidy/cmake/ament_clang_tidy.cmake
+++ ament_cmake_clang_tidy/cmake/ament_clang_tidy.cmake
@@ -68,7 +68,7 @@ function(ament_clang_tidy)
   endif()
 
   if(NOT ARG_TIMEOUT)
-    set(ARG_TIMEOUT 600)
+    set(ARG_TIMEOUT 1200)
   elseif(DEFINED ament_cmake_clang_tidy_TIMEOUT)
     set(ARG_TIMEOUT "${ament_cmake_clang_tidy_TIMEOUT}")
   endif()
